3053e3586187ba2eb66f33429b520c17cfa05a5c
Sammie-drew <empireandz@gmail.com>
2020-10-20 12:18:17 +0100

    Modifications to the x file and the use of online db

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,8 @@ const winston = require("winston");
 const mongoose = require("mongoose");
 const config = require("config");
 module.exports = function (params) {
-  const db = config.get("db");
+  const db =
+    "mongodb+srv://admin:GKViPnBuUxlQ1Teh@cluster0.otgsx.mongodb.net/vidlydb?retryWrites=true&w=majority";
   mongoose
     .connect(config.get("db"), {
       useUnifiedTopology: true,
